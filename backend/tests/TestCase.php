43d2143ac1bbcd1522cc61f5576ea5a094c5e3be
Patrik Rajnoha <patrikrajnoha@users.noreply.github.com>
2026-04-02 19:00:57 +0200

    test(ci): disable upload moderation scan in base TestCase runtime

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -6,5 +6,10 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        config()->set('moderation.enforce_upload_image_scan', false);
+    }
 }
